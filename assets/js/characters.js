3bb7c8a05794a3d20341cdcea51ec90dbf9b6672
Stef Van Boven <stefvboven2020@outlook.com>
2024-12-24 01:17:41 +0100

    added homepage + styling homepage also added some characters with some styling as well

---
+++ assets/js/characters.js
@@ -0,0 +1,25 @@
+async function loadCharacters() {
+    try {
+        const response = await fetch('./assets/js/data/characters.json');
+        const characters = await response.json();
+
+        const gallery = document.querySelector('.gallery');
+        gallery.innerHTML = ''; // Maak de gallery leeg
+
+        characters.forEach(character => {
+            const image = new Image();
+            image.src = character.image;
+            const card = document.createElement('div');
+            card.classList.add('character-card');
+            card.innerHTML = `
+                    <img src="${character.image}" alt="${character.name}">
+                    <h3>${character.name}</h3>
+                `;
+            gallery.appendChild(card);
+        });
+    } catch (error) {
+        console.error('Fout bij het laden van characters:', error);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadCharacters);
